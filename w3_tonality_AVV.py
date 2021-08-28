0235482c4ad4e5a2e035f4ffcf5caeb6c6a784bf
v010ch <iakhremenko.v@gmail.com>
2021-08-28 23:15:52 +0300

    week3 kagle inclass. pb 0.74000
    + clean data
    + stem
    + lemm
    + gensim word2vec on train data
    + svc
    + tune
    
    
    Signed-off-by: v010ch <iakhremenko.v@gmail.com>

--- w3_tonality_AVV.py
+++ w3_tonality_AVV.py
@@ -9,7 +9,7 @@
 
 # # Kagle inclass https://www.kaggle.com/c/simplesentiment/overview
 
-# In[ ]:
+# In[1]:
 
 
 import os
@@ -18,8 +18,14 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 
+from itertools import product
+import warnings
+from tqdm import tqdm
 
-# In[ ]:
+import re
+
+
+# In[2]:
 
 
 import nltk
@@ -30,19 +36,27 @@ import nltk
 
 import nltk.stem as st
 
-#import re
+
+# In[3]:
 
 
-# In[ ]:
+import spacy
+
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.sklearn_api import W2VTransformer
+
+
+# In[76]:
 
 
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+#from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.model_selection import GridSearchCV, cross_val_score 
@@ -52,19 +66,20 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix
 
 
-# In[ ]:
+# In[45]:
 
 
-#import xgboost as xgb
-
-
-# In[ ]:
-
-
-
+import xgboost as xgb
+from lightgbm import LGBMClassifier
 
 
 # In[ ]:
+
+
+
+
+
+# In[6]:
 
 
 PATH_DATA = os.path.join(Path.cwd(), 'data')
@@ -83,7 +98,7 @@ PATH_SUBM = os.path.join(Path.cwd(), 'submissions')
 
 
 
-# In[ ]:
+# In[8]:
 
 
 df = pd.read_csv(os.path.join(PATH_DATA, 'products_sentiment_train.tsv'), 
@@ -95,13 +110,13 @@ df.columns = ['text', 'target']
 df.shape
 
 
-# In[ ]:
+# In[9]:
 
 
 df.head()
 
 
-# In[ ]:
+# In[10]:
 
 
 df.target.value_counts()
@@ -116,7 +131,7 @@ df.target.value_counts()
 
 # ### Приводим к строчным (на всякий) и очищаем по порядку:
 
-# In[ ]:
+# In[11]:
 
 
 # - все спецсимволы
@@ -124,7 +139,7 @@ df.target.value_counts()
 # - заменяем множественные пробелы на единичные
 
 
-# In[ ]:
+# In[12]:
 
 
 clean_text = lambda x: re.sub(r"\s+", ' ', 
@@ -134,7 +149,7 @@ clean_text = lambda x: re.sub(r"\s+", ' ',
                              )
 
 
-# In[ ]:
+# In[13]:
 
 
 df['text_cl'] = df.text.map(clean_text)
@@ -146,19 +161,19 @@ df['text_cl'] = df.text.map(clean_text)
 
 
 
-# In[ ]:
+# In[14]:
 
 
+#tagged_data[:5]
 
 
-
-# In[ ]:
+# In[15]:
 
 
 #lem_text("don't"), lem_text("i'll")
 
 
-# In[ ]:
+# In[16]:
 
 
 #stem_text("don't"), stem_text("i'll")
@@ -166,23 +181,18 @@ df['text_cl'] = df.text.map(clean_text)
 
 # ### Лемматизация & стемминг
 
-# In[ ]:
+# In[17]:
 
 
 lemm = st.WordNetLemmatizer()
 lem_text = lambda x: ' '.join([lemm.lemmatize(el) for el in x.split()])
 
 
-#stemm = st.PorterStemmer()
-#stemm = st.Cistem()
-#stemm = st.LancasterStemmer()
-
-#stemm = st.ISRIStemmer()
 stemm = st.RSLPStemmer()
 stem_text = lambda x: ' '.join([stemm.stem(el) for el in x.split()])
 
 
-# In[ ]:
+# In[18]:
 
 
 df['text_cl'] = df.text.map(clean_text)
@@ -190,15 +200,33 @@ df['text_cl'] = df.text_cl.map(lem_text)
 df['text_cl'] = df.text_cl.map(stem_text)
 
 
-# In[ ]:
+# In[19]:
+
+
+df['text_cl'] = df.text_cl.map(lambda x: x.split())
+
+
+# In[20]:
 
 
 df.head()
 
 
-# ### Подготавливаем к обучению по сетке
+# In[ ]:
+
+
+
+
 
 # In[ ]:
+
+
+
+
+
+# ### Подготавливаем к обучению по сетке
+
+# In[21]:
 
 
 train, test, train_target, test_target = train_test_split(df.text_cl, df.target, 
@@ -208,99 +236,110 @@ train, test, train_target, test_target = train_test_split(df.text_cl, df.target,
                                                          )
 
 
-# ### параметры для перебора
-
 # In[ ]:
 
 
-pipe_cnt = Pipeline([('vect', CountVectorizer()), 
-                     ('clf', SGDClassifier()),
+
+
+
+# ### Параметры для перебора
+
+# In[24]:
+
+
+pipe_cnt = Pipeline([#('w2v', W2VTransformer(size=100, min_count=1, seed=1)),
+                     ('clf', LogisticRegression()),
                       ])
 
 
-# In[ ]:
+# In[25]:
 
 
-parameters = [
-    {
-        'vect': [CountVectorizer(), TfidfVectorizer()],
-        #'vect__analyzer': ['word','char_wb'],
-        'vect__analyzer': ['char_wb'],
-        #'vect__ngram_range': [(1, 1), (1, 2), (2, 2), (2, 3), (2, 4), (3, 4), (3, 5), (2, 5)],
-        'vect__ngram_range': [(2, 4), (3, 4), (3, 5), (2, 5)],
-        'vect__stop_words': [None, nltk.corpus.stopwords.words('english')],
-        'vect__max_df': [0.75, 1.0],
-        'vect__min_df': [10, 1,],# 50],
+parameters_v0 = {
         'clf': [LogisticRegression(), 
                 LinearSVC(),
-                SGDClassifier(), 
-                RandomForestClassifier()
+                #SGDClassifier(), 
+                #RandomForestClassifier(),
                 #xgb.XGBClassifier(tree_method='gpu_hist', gpu_id=0),
                 #xgb.XGBClassifier(eval_metric = 'auc'),
                ],
-    },
-    
+    }
+
+
+# In[60]:
+
+
+parameters = [
+            {'clf': [LogisticRegression()]}, 
+            {'clf': [LinearSVC(max_iter = 1500)]},
+            #{'clf': [SGDClassifier()]}, 
+            #{'clf': [RandomForestClassifier()]},
+            #{'clf': [xgb.XGBClassifier(eval_metric = 'auc', use_label_encoder=False)]},
+                     #xgb.XGBClassifier(tree_method='gpu_hist', gpu_id=0),
+            #{'clf': [LGBMClassifier()]},
 ]
 
 
-# In[ ]:
+# In[63]:
 
 
-grid = GridSearchCV(pipe_cnt, parameters, cv = 5, scoring = 'roc_auc', verbose = 2, n_jobs=-1)
+par_size = [300, 200,]# 100, 50]#10, 20, 30, 50, ]
+par_window = [5,]# 4, 3,]# 2]
+#par_hs = [1,]# 0]
+par_niter = [50, 40,]# 30,]# 75,]# 100]#3, 5, 10, 50]
+par_min_count = [5, 3,]# 10, 2, 1]
+len(list(product(par_size, 
+                 par_window, 
+                 #par_hs, 
+                 par_niter, 
+                 par_min_count)))
 
 
-# In[ ]:
+# In[97]:
 
 
-get_ipython().run_cell_magic('time', '', 'grid.fit(train, train_target)\ngrid.best_estimator_')
+# Трансфорование данных (отзыва) в вектор
+# требуется исключить слова, которые не встречались, т.к. выдаст ошибку
+# и полученные вектораотзыва усредняем по столбцам
+def transform_x2v(inp_model, inp_series):
+    transformed_ndarray = np.ndarray((inp_series.shape[0], inp_model.gensim_model.vector_size))
+    
+    for row, idx in enumerate(inp_series.index):
+        
+        tr_string = ''
+        for wrd in inp_series[idx]:
+            if wrd in inp_model.gensim_model.wv.vocab:
+                tr_string = ' '.join([tr_string, wrd])
+
+        transformed_ndarray[row] = np.mean(inp_model.transform(tr_string.split()), axis = 0)
+    
+    return transformed_ndarray
 
 
-# In[ ]:
+# In[68]:
 
 
-grid.best_params_
-
-
-# In[ ]:
-
-
-grid.best_score_
-
-
-# In[ ]:
-
-
-grid.cv_results_.keys()
+get_ipython().run_cell_magic('time', '', 'warnings.filterwarnings("ignore")\n\n\nscores = [(\'model\', (0, 0, 0, 0), 0) for idx in range(5)]\nfor (sz, wind, it, mc) in tqdm(product(par_size, par_window, par_niter, par_min_count), \n                              total = len(list(product(par_size, par_window, par_niter, par_min_count)))):\n    model_x2v = W2VTransformer(size=sz, window = wind, hs = 1, iter = it, min_count = mc,\n                               seed = 16544873\n                              )\n    #model_x2v.fit(train)\n    model_x2v.fit(df.text_cl)\n    #train_x2v = transform_x2v(model_x2v, train)\n    train_x2v = np.nan_to_num(transform_x2v(model_x2v, train))\n\n    grid = GridSearchCV(pipe_cnt, parameters, cv = 5, scoring = \'roc_auc\', verbose = 0, n_jobs=-1)\n    ret = grid.fit(train_x2v, train_target)\n    if grid.best_score_ > scores[4][2]:\n        scores[4] = (grid.best_estimator_[\'clf\'].__class__, (sz, wind, it, mc), grid.best_score_)\n        scores.sort(key=lambda tup: tup[2], reverse = True)\n\n\n\n#warnings.filterwarnings("always")\nwarnings.filterwarnings("default")')
 
 
 # Посмотрим на несколько лучших результатов
 
-# In[ ]:
+# In[69]:
 
 
-#grid.cv_results_['rank_test_score']
+scores
 
-
-# In[ ]:
-
-
-interested = np.where(grid.cv_results_['rank_test_score'] <= 5)
-for field in parameters[0].keys():
-    print(field)
-    for idx in interested:
-        print(grid.cv_results_['param_' + field][idx])
-
-
-# Промежуточные выводы:
-# - токенизация всегда TfidfVectorizer
-# - метод разбиения - char_wb
-# - ngram_range - (3, 4) для 1х и 3х результатов, (3,5) для 5х.
-#    включу ngram_range в перебор при настройке параметров
-# - модель - LogisticRegression
-# - min_df - всегда 1
-# - max_df - 0.75 для 3х результатов, 1.0 для 1х и 5х. 
-#    включу max_df в перебор при настройке параметров
-# - стоп слова не влияют никаким образом (что очень удивительно)
+[(sklearn.svm._classes.LinearSVC, (200, 5, 50, 5), 0.8453186181634458),
+ (sklearn.svm._classes.LinearSVC, (300, 5, 50, 5), 0.8447524810100875),
+ (sklearn.svm._classes.LinearSVC, (200, 5, 40, 5), 0.8438951796456868),
+ (sklearn.svm._classes.LinearSVC, (300, 5, 40, 5), 0.8374836539390291),
+ (sklearn.svm._classes.LinearSVC, (300, 5, 50, 3), 0.8334031755938449)]
+# Промежуточные выводы /параметры для W2VTransformer:
+# - размер embedded вектора - 200
+# - max расстояние между словами всегда 5
+# - hs всегда 1
+# - кол-во итерций построения словаря - 50
+# - модель - LinearSVC
 # 
 
 # In[ ]:
@@ -311,31 +350,28 @@ for field in parameters[0].keys():
 
 # ### Попробуем определить причины ошибок по тесту
 
-# In[ ]:
+# In[70]:
 
 
-vectorizer = TfidfVectorizer(analyzer='char_wb', ngram_range=(3, 4), 
-                             max_df=1.0, min_df=1, 
-                             stop_words=None
-                            )
-vectorizer.fit(train)
+model_x2v = W2VTransformer(size=200, window = 5, hs = 1, iter = 50, min_count = 5,
+                           seed = 16544873
+                          )
+#model_x2v.fit(train)
+model_x2v.fit(df.text_cl)
+#train_x2v = transform_x2v(model_x2v, train)
+train_x2v = np.nan_to_num(transform_x2v(model_x2v, train))
+
+model = LinearSVC(max_iter = 2500)
+model.fit(train_x2v, train_target)
 
 
-# In[ ]:
+# In[73]:
 
 
-model = LogisticRegression(
-)
-model.fit(vectorizer.transform(train), train_target)
+test_pred = model.predict(np.nan_to_num(transform_x2v(model_x2v, test)))
 
 
-# In[ ]:
-
-
-test_pred = model.predict(vectorizer.transform(test))
-
-
-# In[ ]:
+# In[74]:
 
 
 confusion_matrix(test_target, test_pred)
@@ -343,7 +379,7 @@ confusion_matrix(test_target, test_pred)
 
 # только половина отрицательных отзывов, посмотрим их
 
-# In[ ]:
+# In[75]:
 
 
 for idx in range(test.shape[0]):
@@ -352,8 +388,8 @@ for idx in range(test.shape[0]):
 
 
 # Если посмотреть на отзывы, в которых модель ошиблась - то в них нет ничего необычного.   
-# Большая часть однозначно определяется как положителбные или отрицательные.   
-# Предполагаю, что влияют 2 фактора: несбалансированность класса и предел возможностей tf-idf   
+# Большая часть однозначно определяется как положительные или отрицательные.   
+# Предполагаю, что влияют 2 фактора: несбалансированность класса и не больша выборка для обучения
 # Попробуем исправить, настроив class_weights
 
 # In[ ]:
@@ -367,58 +403,47 @@ for idx in range(test.shape[0]):
 # #### в 3-4 итерации. в начале ищем приближенные лучшие значения(закомментированные параметры ниже),   
 # #### потом их уточняем
 
-# In[ ]:
+# In[87]:
 
 
-pipe_cnt = Pipeline([('vect', TfidfVectorizer(analyzer='char_wb')), 
-                     ('clf', LogisticRegression()),
+pipe_cnt = Pipeline([('clf', SVC()),
                       ])
 
 
-# In[ ]:
+# In[115]:
 
 
 model_parameters = {
-    'vect__ngram_range': [(3, 4), ], #(3, 5)],
-    #'vect__max_df': [0.75, 0.65, 0.85,], #1.0],
-    'vect__max_df': [0.75],
-    'clf__solver': ['liblinear',], #'lbfgs'],
-    'clf__penalty': ['l2',], #'l1'], 
-    #'clf__C': np.logspace(1, 5, 10),
-    'clf__C': np.linspace(1.69, 1.74, 20),
+    'clf__max_iter' : [2500, 2000],# 1500],
+    'clf__kernel': ['linear'],# 'poly', 'rbf', 'sigmoid',],
+    #'clf__C': np.logspace(-2, 2, 10),
+    'clf__C': np.linspace(1.7, 2, 40),
     #'clf__class_weight': [{0: 0.5 - shift, 1:0.5 + shift} for shift in np.linspace(-0.1, 0.1, 20)],# + ['balanced'],
-    'clf__class_weight': [{0: 0.59 - shift, 1:0.41 + shift} for shift in np.linspace(-0.001, 0.001, 50)],# + ['balanced'],
-    'clf__max_iter': [50, 75],# 100, 200],
+    'clf__class_weight': [{0: 0.56 - shift, 1:0.44 + shift} for shift in np.linspace(-0.01, 0.01, 20)]# + ['balanced'],
 }
 
 
-# In[ ]:
+# In[133]:
 
 
-grid_tune = GridSearchCV(pipe_cnt, model_parameters, cv = 5, scoring = 'roc_auc', verbose = 2, n_jobs=-1)
+get_ipython().run_cell_magic('time', '', "\nmodel_x2v = W2VTransformer(size=200, window = 5, hs = 1, iter = 50, min_count = 5,\n                           seed = 16544873\n                          )\nmodel_x2v.fit(df.text_cl)\ntrain_x2v = np.nan_to_num(transform_x2v(model_x2v, train))\n\ngrid_tune = GridSearchCV(pipe_cnt, model_parameters, cv = 5, scoring = 'roc_auc', verbose = 1, n_jobs=-1)\ngrid_tune.fit(train_x2v, train_target)\ngrid_tune.best_estimator_")
 
 
-# In[ ]:
-
-
-get_ipython().run_cell_magic('time', '', 'grid_tune.fit(train, train_target)\ngrid_tune.best_estimator_')
-
-
-# In[ ]:
+# In[145]:
 
 
 grid_tune.best_params_
 
 
-# In[ ]:
+# In[146]:
 
 
 grid_tune.best_score_
 
-
+0.842950668330993
 # Посмотрим на несколько лучших результатов, после чего уточняем параметры и повторяем (несколько раз) GridSearchCV
 
-# In[ ]:
+# In[136]:
 
 
 interested_tune = np.where(grid_tune.cv_results_['rank_test_score'] <= 5)
@@ -428,25 +453,31 @@ for field in model_parameters.keys():
         print(grid_tune.cv_results_['param_' + field][idx])
 
 
-# In[ ]:
+# In[137]:
 
 
 grid_tune.cv_results_['mean_test_score'][np.where(grid_tune.cv_results_['rank_test_score'] <= 5)]
 
+array([0.84258561, 0.84246868, 0.84295067, 0.84274984, 0.84246112])
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
 
 # Остановимся на следующих параметрах:    
-# TfidfVectorizer    
-# analyzer ='char_wb'    
-# max_df = 0.75    
-# ngram_range = (3, 4)    
 #     
-# LogisticRegression   
-# C =  1.7189473684210526   
-# сlass_weight = {0: 0.5894081632653061, 1: 0.41059183673469385}   
-# max_iter = 50 (возможно, мало итераций т.к. обучались не на всех данных, так что для submission можно попробовать и 75)   
-# penalty = 'l2'   
-# solver = 'liblinear'   
-#  
+# SVC
+# kernel = 'linear'
+# C =  2.41025641025641
+# сlass_weight = {0: 0.5246153846153846, 1: 0.47538461538461535}
+# max_iter = 2000
 
 # Получим модель и submission для выбранных параметров
 
@@ -458,7 +489,7 @@ grid_tune.cv_results_['mean_test_score'][np.where(grid_tune.cv_results_['rank_te
 
 # ### Подготавливаем данные для обучения финальной модели и прогноза
 
-# In[ ]:
+# In[147]:
 
 
 df_test = pd.read_csv(os.path.join(PATH_DATA, 'products_sentiment_test.tsv'),
@@ -468,43 +499,38 @@ df_test = pd.read_csv(os.path.join(PATH_DATA, 'products_sentiment_test.tsv'),
 df_test.shape
 
 
-# In[ ]:
+# In[148]:
 
 
 #df_test.head(), df_test.tail()
 
 
-# In[ ]:
+# In[149]:
 
 
 subm = pd.read_csv(os.path.join(PATH_DATA, 'products_sentiment_sample_submission.csv'))
 subm.shape
 
 
-# In[ ]:
+# In[150]:
 
 
 #subm.head(), subm.tail()
 
 
-# In[ ]:
-
-
-vectorizer = TfidfVectorizer(analyzer='char_wb', ngram_range=(3, 4), 
-                             max_df=0.75, min_df=1, 
-                             stop_words=None
-                            )
-vectorizer.fit(df.text_cl)
-train_df = vectorizer.transform(df.text_cl)
-
-
-# In[ ]:
+# In[151]:
 
 
 test_df = df_test.text.map(clean_text)
 test_df = test_df.map(lem_text)
 test_df = test_df.map(stem_text)
-test_df = vectorizer.transform(test_df)
+test_df = test_df.map(lambda x: x.split())
+
+
+# In[152]:
+
+
+test_x2v = np.nan_to_num(transform_x2v(model_x2v, test_df))
 
 
 # In[ ]:
@@ -518,40 +544,29 @@ test_df = vectorizer.transform(test_df)
 # In[ ]:
 
 
-model = LogisticRegression(solver = 'liblinear',   
-                           penalty = 'l2',
-                           C = 1.7189473684210526,
-                           class_weight = {0: 0.5894081632653061, 1: 0.41059183673469385},
-                           max_iter = 75, #75
-)
 
 
-# In[ ]:
+
+# In[153]:
 
 
-model.fit(train_df, df.target)
-
-
-# In[ ]:
-
-
-pred = model.predict(test_df)
+pred = grid_tune.best_estimator_.predict(test_x2v)
 subm.y = pred
 
 
+# In[154]:
+
+
+subm.to_csv(os.path.join(PATH_SUBM, 'gensim_w2v_own_lsvc_tuned.csv'), index = False)
+
+
 # In[ ]:
 
 
-subm.to_csv(os.path.join(PATH_SUBM, 'tf_st_lm_lr_tuned_75.csv'), index = False)
-
-
-# In[ ]:
 
 
 
-
-
-# pb 0.79333
+# pb 0.74000
 
 # In[ ]:
 
